8ee19d87475597836e83b14b8d21f42cce6be340
mercysunday <mirandaeswift686@gmail.com>
2023-09-15 02:10:30 -0700

    My betty file

---
+++ Betty.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: always return 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 5; i++)
+	{
+		printf("Hello Betty \n");
+	}
+	
+	return (0);
+}
